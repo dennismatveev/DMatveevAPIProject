4ca2b7ae1bd72dab9891fe85e5184103b095540a
Dennis <dennis.matveev711@gmail.com>
2021-02-02 10:07:00 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+def get_data(url: str):
+    all_data = []
+
+
+    return all_data
+
+
+def main():
+    url = ""
+    all_data = get_data(url)
+
+
+if __name__== '__main__':
+    main()
