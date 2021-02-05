2aec55ed77cdcbb8f365ed1e8b5d43c3e8d90209
Dennis <dennis.matveev711@gmail.com>
2021-02-05 12:03:44 -0500

    Added:  fields to search,  pages to loop through,  commments

--- main.py
+++ main.py
@@ -2,21 +2,34 @@ import requests
 import secrets
 
 
+
 def get_data(url: str):
-    all_data = [] #this will hold the return value
-    full_url = f"{url}&api_key={secrets.api_key}&page=3"
-    response = requests.get(full_url)
-    if response.status_code != 200:
-        print(response.text)
-        return []
-    json_data = response.json()
-    results = json_data['results']
-    all_data.extend(results)
+    # Which fields are desired in the output
+    fields = ["school.name,", "school.city,", "2018.student.size,", "2017.student.size,",
+              "2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,",
+              "2016.repayment.3_yr_repayment.overall"
+              ]
+    # this will hold the return value
+    all_data = []
+
+    # Convert List to  String
+    field_string = ''.join(fields)
+
+    for pages in range(0, 161):
+        full_url = f"{url}{field_string}&api_key={secrets.api_key}&page={pages}"
+        response = requests.get(full_url)
+        if response.status_code != 200:
+            print(response.text)
+            return []
+        json_data = response.json()
+        results = json_data['results']
+        all_data.extend(results)
     return all_data
 
 
 def main():
-    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.name,2013.student.size"
+
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields="
     all_data = get_data(url)
     for item in all_data:
         print(item)
