d94df2de5848365dbe84f073c094e6b30c59c1e3
Dennis <dennis.matveev711@gmail.com>
2021-02-05 23:35:56 -0500

    Added Readme File, and prints data to new file

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
 import secrets
-
+import time
+import math
+import pprint
 
 
 def get_data(url: str):
+    total_items = 3203
+    items_per_page = 20
     # Which fields are desired in the output
     fields = ["school.name,", "school.city,", "2018.student.size,", "2017.student.size,",
               "2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,",
@@ -15,7 +19,7 @@ def get_data(url: str):
     # Convert List to  String
     field_string = ''.join(fields)
 
-    for pages in range(0, 161):
+    for pages in range(0, math.ceil(total_items/items_per_page)):
         full_url = f"{url}{field_string}&api_key={secrets.api_key}&page={pages}"
         response = requests.get(full_url)
         if response.status_code != 200:
@@ -24,6 +28,7 @@ def get_data(url: str):
         json_data = response.json()
         results = json_data['results']
         all_data.extend(results)
+        time.sleep(.05)
     return all_data
 
 
@@ -31,8 +36,11 @@ def main():
 
     url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields="
     all_data = get_data(url)
-    for item in all_data:
-        print(item)
+    with open('College_data.txt', 'w') as f:
+        print(all_data, file=f)
+
+    # for item in all_data:
+    #     print(item)
 
 
 
