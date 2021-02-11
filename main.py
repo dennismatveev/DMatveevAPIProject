15428a0b54d9b2e6fa59cd9ceb3e321b09c3012c
Dennis <dennis.matveev711@gmail.com>
2021-02-11 09:36:41 -0500

    Building and creating an empty DB

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import secrets
 import time
 import math
+import sqlite3
+from typing import Tuple
 
 
 def get_data(url: str):
@@ -32,12 +34,55 @@ def get_data(url: str):
     return all_data
 
 
-def main():
+def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    db_connection = sqlite3.connect(filename)#connect to existing DB or create new one
+    cursor = db_connection.cursor()#get ready to read/write data
+    return db_connection, cursor
 
-    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields="
-    all_data = get_data(url)
-    with open('College_data.txt', 'w') as f:
-        print(all_data, file=f)
+
+def close_db(connection:sqlite3.Connection):
+    connection.commit()#make sure any changes get saved
+    connection.close()
+
+def setup_db(cursor:sqlite3.Cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS students(
+    banner_id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    gpa REAL DEFAULT 0,
+    credits INTEGER DEFAULT 0
+    );''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS course(
+    course_prefix TEXT NOT NULL,
+    course_number INTEGER NOT NULL,
+    cap INTEGER DEFAULT 20,
+    description TEXT,
+    PRIMARY KEY(course_prefix, course_number)
+    );''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS class_list(
+       registration_id INTEGER PRIMARY KEY,
+       course_prefix TEXT NOT NULL,
+       course_number INTEGER NOT NULL,
+       banner_id INTEGER NOT NULL,
+       registration_date TEXT,
+       FOREIGN KEY (banner_id) REFERENCES student (banner_id)
+       ON DELETE CASCADE ON UPDATE NO ACTION,
+       FOREIGN KEY (course_prefix, course_number) REFERENCES courses (course_prefix, course_number)
+       ON DELETE CASCADE ON UPDATE NO ACTION
+       );''')
+
+def main():
+    conn, cursor = open_db("demo_db.sqlite")
+    print(type(conn))
+    setup_db(cursor)
+    close_db(conn)
+
+# def main():
+#
+#     url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields="
+#     all_data = get_data(url)
+# dont need    with open('College_data.txt', 'w') as f:
+# dont need        print(all_data, file=f)
 
 
 if __name__ == '__main__':
