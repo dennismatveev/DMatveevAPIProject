8c35129429aca2e7939d7276fd130335212c5ec6
Dennis <dennis.matveev711@gmail.com>
2021-02-16 12:55:27 -0500

    Set up database for university data, all fields are set up too.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import secrets
 import time
 import math
 import sqlite3
+import random
 from typing import Tuple
 
 
@@ -45,36 +46,94 @@ def close_db(connection:sqlite3.Connection):
     connection.close()
 
 def setup_db(cursor:sqlite3.Cursor):
-    cursor.execute('''CREATE TABLE IF NOT EXISTS students(
-    banner_id INTEGER PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    gpa REAL DEFAULT 0,
-    credits INTEGER DEFAULT 0
+    cursor.execute('''CREATE TABLE IF NOT EXISTS University_Data(
+    school_name TEXT NOT NULL PRIMARY KEY,
+    school_city TEXT NOT NULL,
+    student_size_2018 INTEGER DEFAULT 0,
+    student_size_2017 INTEGER DEFAULT 0,
+    earnings_after3yearscompletion_2017 INTEGER DEFAULT 0,
+    repayment_3years_2016 INTEGER DEFAULT 0
     );''')
-    cursor.execute('''CREATE TABLE IF NOT EXISTS course(
-    course_prefix TEXT NOT NULL,
-    course_number INTEGER NOT NULL,
-    cap INTEGER DEFAULT 20,
-    description TEXT,
-    PRIMARY KEY(course_prefix, course_number)
-    );''')
-    cursor.execute('''CREATE TABLE IF NOT EXISTS class_list(
-       registration_id INTEGER PRIMARY KEY,
-       course_prefix TEXT NOT NULL,
-       course_number INTEGER NOT NULL,
-       banner_id INTEGER NOT NULL,
-       registration_date TEXT,
-       FOREIGN KEY (banner_id) REFERENCES student (banner_id)
-       ON DELETE CASCADE ON UPDATE NO ACTION,
-       FOREIGN KEY (course_prefix, course_number) REFERENCES courses (course_prefix, course_number)
-       ON DELETE CASCADE ON UPDATE NO ACTION
-       );''')
+
+    # cursor.execute('''CREATE TABLE IF NOT EXISTS students(
+    # banner_id INTEGER PRIMARY KEY,
+    # first_name TEXT NOT NULL,
+    # last_name TEXT NOT NULL,
+    # gpa REAL DEFAULT 0,
+    # credits INTEGER DEFAULT 0
+    # );''')
+    # cursor.execute('''CREATE TABLE IF NOT EXISTS course(
+    # course_prefix TEXT NOT NULL,
+    # course_number INTEGER NOT NULL,
+    # cap INTEGER DEFAULT 20,
+    # description TEXT,
+    # PRIMARY KEY(course_prefix, course_number)
+    # );''')
+    # cursor.execute('''CREATE TABLE IF NOT EXISTS class_list(
+    #    registration_id INTEGER PRIMARY KEY,
+    #    course_prefix TEXT NOT NULL,
+    #    course_number INTEGER NOT NULL,
+    #    banner_id INTEGER NOT NULL,
+    #    registration_date TEXT,
+    #    FOREIGN KEY (banner_id) REFERENCES student (banner_id)
+    #    ON DELETE CASCADE ON UPDATE NO ACTION,
+    #    FOREIGN KEY (course_prefix, course_number) REFERENCES courses (course_prefix, course_number)
+    #    ON DELETE CASCADE ON UPDATE NO ACTION
+    #    );''')
+
+def make_initial_students(cursor: sqlite3.Cursor):
+    cursor.execute(f'''INSERT INTO STUDENTS (banner_id, first_name, last_name, gpa, credits)
+            VALUES (1001, "John", "Santore", ?, ?)''', (random.uniform(0.0, 4.0), random.randint(0, 120))) # This is important- use with loop
+    cursor.execute(f'''INSERT INTO STUDENTS(banner_id, first_name, last_name, gpa, credits)
+            VALUES(1002, "Enping", "Li", {random.uniform(0.0, 4.0)}, {random.randint(0, 120)})''')
+    cursor.execute(f'''INSERT INTO STUDENTS(banner_id, first_name, last_name, gpa, credits)
+            VALUES(1003, "Michael", "Black", {random.uniform(0.0, 4.0)}, {random.randint(0, 120)})''')
+    cursor.execute(f'''INSERT INTO STUDENTS(banner_id, first_name, last_name, gpa, credits)
+            VALUES(1004, "Seikyung", "Jung", {random.uniform(0.0, 4.0)}, {random.randint(0, 120)})''')
+    cursor.execute(f'''INSERT INTO STUDENTS(banner_id, first_name, last_name, gpa, credits)
+            VALUES(1005, "Haleh", "Khojasteh", {random.uniform(0.0, 4.0)}, {random.randint(0, 120)})''')
+
+def make_initial_courses(cursor:sqlite3.Cursor):
+    cursor.execute(f'''INSERT INTO COURSE (course_prefix, course_number, cap, description)
+        VALUES ('COMP', 151, 24, 'This is the intro course, you will learn to program, maybe for the first time')''')
+    cursor.execute(f'''INSERT INTO COURSE (course_prefix, course_number, cap, description)
+        VALUES ('COMP', 490, 20, 'This is the final course. You will get a chance to apply much of what you learned troughout the undergrad degree')''')
+    cursor.execute(f'''INSERT INTO COURSE (course_prefix, course_number, cap, description)
+        VALUES ('MATH', 130, 20, 'This course is changing to include much more on graph theory and number bases/systems')''')
+
+def make_initial_classLists(cursor: sqlite3.Cursor):
+    cursor.execute(f'''INSERT INTO CLASS_LIST (banner_id, course_prefix, course_number,  registration_date)
+    VALUES(1001, 'Comp', 490, DATE('now'))
+    ''')
+    cursor.execute(f'''INSERT INTO CLASS_LIST (banner_id, course_prefix, course_number,  registration_date)
+        VALUES(1002, 'Comp', 490, DATE('now'))
+        ''')
+    cursor.execute(f'''INSERT INTO CLASS_LIST (banner_id, course_prefix, course_number,  registration_date)
+            VALUES(1003, 'Comp', 490, DATE('now'))
+            ''')
+    cursor.execute(f'''INSERT INTO CLASS_LIST (banner_id, course_prefix, course_number,  registration_date)
+            VALUES(1004, 'Comp', 490, DATE('now'))
+            ''')
+    cursor.execute(f'''INSERT INTO CLASS_LIST (banner_id,  course_prefix, course_number, registration_date)
+            VALUES(1005, 'Comp', 490, DATE('now'))
+            ''')
+
+def show_simple_select(cursor:sqlite3.Cursor):
+    cutoff = float(input("What should the GPA cutoff be?"))
+    #question to class-what about security issues here? 	       #Discuss
+    result = cursor.execute(f'SELECT * from STUDENTS WHERE gpa < {cutoff}')
+    for row in result:
+        print(f'BannerId: {row[0]}\nName: {row[1]}\
+             {row[2]}\nGPA:{row[3]}')
 
 def main():
     conn, cursor = open_db("demo_db.sqlite")
-    print(type(conn))
+    #print(type(conn))
+    #make_initial_students(cursor)
+    #make_initial_courses(cursor)
+    #make_initial_classLists(cursor)
     setup_db(cursor)
+    #show_simple_select(cursor)
     close_db(conn)
 
 # def main():
