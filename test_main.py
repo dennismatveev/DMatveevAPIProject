14b5ae407a064c325b4fd0896bbc46de0b4734ef
Dennis <dennis.matveev711@gmail.com>
2021-02-23 16:52:44 -0500

    Finished 2nd and 3rd test. To see if New table exists as well as if old table is still in the database. Also added comments to clarify both the two tests

--- test_main.py
+++ test_main.py
@@ -23,7 +23,7 @@ def test_university_test():  # Choose a random target unique id, create the new 
     assert data and (target_unique_id == data[0][0])  # Checking if data array exists, if yes then also check actual id
 
 
-def test_data_acquired():
+def test_data_acquired():  # Tests if data from more than 50 states was obtained
     target_number_states = 50
     states_acquired_xls = []
     workbook = main.openpyxl.load_workbook("CollegeData.xlsx")
@@ -35,12 +35,60 @@ def test_data_acquired():
     assert len(states_acquired_xls) >= target_number_states
 
 
-def test_new_table_created():
-    pass  # Make sure they is a new table
+def test_tables_exist():  # Tests to see if new table is in database
+    target_table = 'XLS_University_Data'
+    api_table_name = 'University_Data'
+    xls_table_name = 'XLS_University_Data'
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields="
+    all_data = main.get_data(url)
+    conn, cursor = main.open_db("demo_db.sqlite")
+
+    main.setup_api_db(cursor, api_table_name)
+    main.populate_api_database(cursor, all_data, api_table_name)
+
+    main.setup_xls_db(cursor, xls_table_name)
+    workbook = main.openpyxl.load_workbook("CollegeData.xlsx")
+    worksheet = workbook.active
+    main.populate_xls_db(cursor, worksheet, xls_table_name)
+
+    cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table'")
+    tables = cursor.fetchall()
+    main.close_db(conn)
+
+    boolean = False  # Traverse through embedded list of tables, and check if desired one is contained
+    for arr in tables:
+        if target_table in arr:
+            boolean = True
+
+    assert tables and boolean  # Checking to see if tables array exists, and if desired table is in the database
 
 
-def test_old_table_exists():
-    pass  # Make sure original table is still there
+def test_old_table_exists():  # Tests to see if old table still exists in database
+    target_table = 'University_Data'
+    api_table_name = 'University_Data'
+    xls_table_name = 'XLS_University_Data'
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields="
+    all_data = main.get_data(url)
+    conn, cursor = main.open_db("demo_db.sqlite")
+
+    main.setup_api_db(cursor, api_table_name)
+    main.populate_api_database(cursor, all_data, api_table_name)
+
+    main.setup_xls_db(cursor, xls_table_name)
+    workbook = main.openpyxl.load_workbook("CollegeData.xlsx")
+    worksheet = workbook.active
+    main.populate_xls_db(cursor, worksheet, xls_table_name)
+
+    cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table'")
+    tables = cursor.fetchall()
+    main.close_db(conn)
+
+    boolean = False  # Traverse through embedded list of tables, and check if desired one is contained
+    for arr in tables:
+        if target_table in arr:
+            boolean = True
+
+    assert tables and boolean  # Checking to see if tables array exists, and if desired table is in the database
 
 
 def test_write_to_table():
